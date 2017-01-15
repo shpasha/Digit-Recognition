b90dba7d8d8d7dfd8480751031d6e19c9e3ab475
shpasha <pash-96@mail.ru>
2017-01-15 12:26:16 +0300

    First Commit
    Single-layer perceptron that recognizes painted digits.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(151, 201);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Инициализация";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 219);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Прав. отв.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 235);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(46, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Да";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(169, 219);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Нет";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(169, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Проверить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(220, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 108);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(383, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 264);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 381);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+    public partial class Form1 : Form
+    {
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+      
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+                  
+        }
+
+        bool p = false;
+        static int n = 20, m = 15, cs = 10;
+
+        int[,] input = new int[n, m];
+        int[,,] weights = new int[10,n,m];
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (p)
+            {
+                Pen blackPen = new Pen(Color.Black, 1);
+                Graphics g = Graphics.FromHwnd(pictureBox1.Handle);
+
+                int px = PointToClient(Cursor.Position).X - pictureBox1.Left;
+                int py = PointToClient(Cursor.Position).Y - pictureBox1.Top;
+                if (px < pictureBox1.Width - 1
+                    && px >= 0 &&
+                    py < pictureBox1.Height - 1 &&
+                    py >= 0)
+                {
+                    input[py / cs, px / cs] = 1;
+                }
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < m; j++)
+                        if (input[i, j] == 1)
+                            g.FillRectangle(new SolidBrush(Color.Black),
+                   j * cs,
+                   i * cs, cs, cs);
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            p = true;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            p = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    weights[Convert.ToInt32(label2.Text), i, j] -= input[i,j];
+                }
+            }
+            saveWeights(Convert.ToInt32(label2.Text));
+
+          /*  for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    weights[Convert.ToInt32(textBox1.Text), i, j] += input[i, j]*2;
+                }
+            }
+            saveWeights(Convert.ToInt32(textBox1.Text));
+            */
+
+            check();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            check();
+        }
+
+        private void check()
+        {
+            int[] f = new int[10];
+            int summax = -100, nummax = 0;
+            for (int k = 0; k < 10; k++)
+            {
+                int sum = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < m; j++)
+                    {
+                        sum += weights[k, i, j] * input[i, j];
+                    }
+                }
+                if (sum < 0) f[k] = 0;
+                else
+                f[k] = sum;
+                if (sum >= summax)
+                {
+                    summax = sum;
+                    nummax = k;
+                }
+            }
+            double[] p = new double[10];
+            int s = 0;
+            for (int i = 0; i < 10; i++)
+                s += f[i];
+            listBox1.Items.Clear();
+            for (int i = 0; i < 10; i++)
+            {
+                p[i] = (double)f[i] / s;
+                listBox1.Items.Add(i.ToString() + " " + p[i].ToString());
+            }
+
+            label2.Text = nummax.ToString();
+            textBox1.Text = label2.Text;
+        }
+
+        private void saveWeights(int k) {
+                using (System.IO.StreamWriter file =
+                        new System.IO.StreamWriter(@k.ToString() + ".txt"))
+                {
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < m; j++)
+                        {
+                            file.Write(weights[k, i, j].ToString() + " ");
+                        }
+                        file.WriteLine("");
+                    }
+                }
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    weights[Convert.ToInt32(textBox1.Text), i, j] += input[i,j];
+                }
+            }
+
+           saveWeights(Convert.ToInt16(textBox1.Text));
+           // Init();
+        }
+
+        private void Init()
+        {
+            pictureBox1.Width = m * cs + 1;
+            pictureBox1.Height = n * cs + 1;
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < m; j++)
+                    input[i, j] = 0;
+
+            Pen blackPen = new Pen(Color.Black, 1);
+
+            Bitmap bmp;
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            g.FillRectangle(new SolidBrush(Color.White), 0, 0, pictureBox1.Width, pictureBox1.Height);
+
+            for (int i = 0; i <= pictureBox1.Width; i += cs)
+                g.DrawLine(blackPen, i, 0, i, pictureBox1.Height);
+
+            for (int j = 0; j <= pictureBox1.Height; j += cs)
+                g.DrawLine(blackPen, 0, j, pictureBox1.Height, j);
+
+            string[] lines;
+            for (int k = 0; k < 10; k++)
+            {
+                using (System.IO.StreamReader file =
+                        new System.IO.StreamReader(@k.ToString() + ".txt"))
+                {
+                    for (int i = 0; i < n; i++)
+                {
+                        lines = file.ReadLine().Split(' ');
+                        for (int j = 0; j < m; j++)
+                        {
+                            weights[k, i, j] = Convert.ToInt32(lines[j]);
+                        }
+                    }
+                }
+            }
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+    }
+}
